aeacc083e34574764ad253b46f189d37b87af371
Gianfranco Savino <81514922+giansav@users.noreply.github.com>
2025-03-11 14:00:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Funzione per mostrare i form
 function showForm(formId) {
-	document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
-	document.getElementById(formId).classList.add("active");
+    // Rimuovi la classe "active" da tutti i form
+    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
+    
+    // Aggiungi la classe "active" solo al form che viene selezionato
+    document.getElementById(formId).classList.add("active");
+}
