78dc5791c9870947dff4a211ae022920bee3126c
Gianfranco Savino <81514922+giansav@users.noreply.github.com>
2025-03-13 09:25:29 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php
 
 $host = "localhost";
+$port = 3307;
 $user = "root";
-$password = "";
+$password = "Giafrik_1";
 $database = "users_db";
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if($conn->connect_error) {
-  die("Coneessione fallita: ". $conn->connect_error);
+  die("Connessione fallita: ". $conn->connect_error);
 }
 
 ?>
